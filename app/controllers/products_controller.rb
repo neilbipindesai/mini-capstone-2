0334b8a9510fad7b4db2fc6706576c8f2abb8692
neilbipindesai <neildesai38@yahoo.com>
2017-09-05 21:20:01 -0500

    Added the update and destroy methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,48 @@
 class ProductsController < ApplicationController
-  def all_products_action
+  def index
     @products = Product.all
-    render 'all_products_page.html.erb'
   end
+
+  def new
+  
+  end
+
+  def create
+    product = Product.new(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+    product.save
+
+    flash[:success] = "New Brand of Ciggerette Successfully Created"
+    redirect_to "/products/#{product.id}"
+
+
+  end
+
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+
+  end
+
+  def update
+    product = Product.find(params[:id])
+    
+    product.assign_attributes(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+
+    product.save
+    flash[:success] = "Brand of Ciggerette Successfully Updated"
+    redirect_to "/products/#{product.id}"
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    flash[:warning] = "Brand of Ciggerette Successfully Destroyed"
+    redirect_to "/products"
+
+  end
+
 end
